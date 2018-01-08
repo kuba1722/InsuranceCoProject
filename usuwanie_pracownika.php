de775f4e3176c61cfc102ce8875e0415fc84f4f5
kuba1722 <32569941+kuba1722@users.noreply.github.com>
2018-01-08 13:20:47 +0100

    Add files via upload

---
+++ usuwanie_pracownika.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Dodaj ekran</title>
+</head>
+<body>
+	<?php
+		require_once "funkcje.php";
+		$polaczenie = Polacz();
+		if ( isset( $_POST['usun'] ) )
+		{
+			$data_missing = array();
+				
+			//sprawdzenie poprawności wprowadzonych danych
+			if( empty( $_POST['typ1'] ) ) $data_missing[] = 'typ1';
+				else $typ1 = trim( $_POST['typ1'] );
+				
+			if ( empty( $data_missing ) )
+			{
+				Usuwanie_pracownikow($polaczenie,$typ1);
+			}
+			else
+			{
+				echo '<font color="red">Musisz uzupełnić następujące pola:</font><br />';
+					
+				foreach( $data_missing as $missing )
+					echo "$missing<br />";
+					
+				echo "<br />";
+			}
+		}	
+		if ( isset( $_POST['menu'] ) )
+		{
+			header("Location: index.html");
+		}
+	?>
+	
+	<form action="usuwanie_pracownika.php" method="post">
+	<table>
+		<b>Usuwanie:</b><br>
+		<tr>
+			<p><td>Podzaj ID klienta do usuniecia :</td> 
+				<td> <input type="int" name="typ1" size="30" value="" /></td>  
+			</p>
+		</tr>
+		
+		<tr>
+		<p>
+			<td><input type="submit" name="usun" value="Usun" />
+				<input type="submit" name="menu" value="Menu" /></td>
+		</p>
+		</tr>
+	</table>	
+	<table>
+	</form>
+		<table width="500" align="center" border="1" bordercolor="#d5d5d5"  cellpadding="0" cellspacing="0">
+        <tr>
+			<?php
+					$rezultat = Pobierz_pracownikow($polaczenie);
+					Wyswitl($rezultat);
+			?>
+		</tr>
+	</table>
+	
+</body>
+</html>
